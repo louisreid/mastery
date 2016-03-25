39a6649f56574b92d9bc7f99247b001eab2dcfd0
Louis Reid <louisreid@holly.local>
2016-03-25 18:11:08 +0000

    Changed mobile styles. Fixed safari video aspect ratio issue

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 $(document).foundation();
+
+$(document).ready(function() {
+	$(".product-hero__video").css("display","none");
+	$(".product-hero__video").css("display","block");
+});
---
+++ js/min/app-min.js
@@ -0,0 +1,1 @@
+$(document).foundation(),$(document).ready(function(){$(".product-hero__video").css("display","none"),$(".product-hero__video").css("display","block")});
